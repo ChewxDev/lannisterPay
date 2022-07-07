b2f6d3a5cb2cf8805e71ea436fec3e3459ebf0c5
Nick njoku <nicholascents77@gmail.com>
2022-07-07 06:55:59 +0100

    updated api route

--- server.js
+++ server.js
@@ -2,28 +2,26 @@ require("express-async-errors");
 const app = require("express")();
 const { PORT } = process.env ?? 8080;
 
-
 // Pre-route middlewares
 require("./src/middlewares/pre-route.middleware")(app);
 
 // API routes
-app.use("/api", require("./src/routes"));
+app.use("", require("./src/routes"));
 
 // Ping route for testing connection
-app.get("/ping", (req, res) => res.status(200).send("Hello world!"));
+app.get("/", (req, res) => res.status(200).send("Hello world!"));
 
 // Error middlewares
 require("./src/middlewares/error.middleware")(app);
 
 // Listen to server port
 app.listen(PORT, async () => {
-     console.log(PORT)
+  console.log(PORT);
 });
 
 // On  server error
 app.on("error", (error) => {
-     console.error(`<::: An error occurred on the server: \n ${error}`);
+  console.error(`<::: An error occurred on the server: \n ${error}`);
 });
 
-
-module.exports = app
+module.exports = app;
