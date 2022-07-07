e7f56a192fdb9892d9f5a35b8eb68d3e5394ad65
Nick njoku <nicholascents77@gmail.com>
2022-07-07 07:01:08 +0100

    final commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("express-async-errors");
 const app = require("express")();
 const { PORT } = process.env ?? 8080;
 
+
 // Pre-route middlewares
 require("./src/middlewares/pre-route.middleware")(app);
 
@@ -9,19 +10,18 @@ require("./src/middlewares/pre-route.middleware")(app);
 app.use("", require("./src/routes"));
 
 // Ping route for testing connection
-app.get("/", (req, res) => res.status(200).send("Hello world!"));
 
 // Error middlewares
 require("./src/middlewares/error.middleware")(app);
 
 // Listen to server port
 app.listen(PORT, async () => {
-  console.log(PORT);
 });
 
 // On  server error
 app.on("error", (error) => {
-  console.error(`<::: An error occurred on the server: \n ${error}`);
+     console.error(`<::: An error occurred on the server: \n ${error}`);
 });
 
-module.exports = app;
+
+module.exports = app
